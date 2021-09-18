fefe3aa3eb92c2a0867bff41cf98c36d23967453
Reto Ryter <reto@twy.gmbh>
2021-09-18 08:56:22 +0000

    chore(test): yeah

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -86,3 +86,216 @@ resource "google_container_node_pool" "primary_nodes" {
 #   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 # }
 
+variable "github_token" {
+  description = "token for github"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "github owner"
+  type        = string
+}
+
+variable "repository_name" {
+  description = "repository name"
+  type        = string
+}
+
+variable "repository_visibility" {
+  description = "repository visibility"
+  default     = "private"
+  type        = string
+}
+
+variable "organization" {
+  description = "organization"
+  type        = string
+}
+
+variable "branch" {
+  description = "branch"
+  type        = string
+  default     = "main"
+}
+
+variable "target_path" {
+  type        = string
+  default     = "l15"
+  description = "Relative path to the Git repository root where the sync manifests are committed."
+}
+
+variable "flux_namespace" {
+  type        = string
+  default     = "flux-system"
+  description = "the flux namespace"
+}
+
+variable "cluster_region" {
+  type        = string
+  description = "cluster region"
+}
+
+variable "use_private_endpoint" {
+  type        = bool
+  description = "Connect on the private GKE cluster endpoint"
+  default     = false
+}
+
+variable "github_deploy_key_title" {
+  type        = string
+  description = "Name of github deploy key"
+}
+
+provider "flux" {}
+
+data "flux_install" "main" {
+  target_path = var.target_path
+}
+
+data "flux_sync" "main" {
+  target_path = var.target_path
+  url         = "ssh://git@github.com/${var.organization}/${var.repository_name}.git"
+  branch      = var.branch
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [google_container_cluster.main]
+
+  create_duration = "30s"
+}
+
+# https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest/submodules/auth
+module "gke_auth" {
+  depends_on           = [time_sleep.wait_30_seconds]
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id           = var.project_id
+  cluster_name         = var.cluster_name
+  location             = var.cluster_region
+  use_private_endpoint = false
+}
+
+provider "kubernetes" {
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  host                   = module.gke_auth.host
+  token                  = module.gke_auth.token
+}
+
+provider "kubectl" {
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  host                   = module.gke_auth.host
+  token                  = module.gke_auth.token
+  load_config_file       = false
+}
+
+# Kubernetes
+resource "kubernetes_namespace" "flux_system" {
+  metadata {
+    name = var.flux_namespace
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+    ]
+  }
+}
+
+data "kubectl_file_documents" "install" {
+  content = data.flux_install.main.content
+}
+
+data "kubectl_file_documents" "sync" {
+  content = data.flux_sync.main.content
+}
+
+locals {
+  install = [for v in data.kubectl_file_documents.install.documents : {
+    data : yamldecode(v)
+    content : v
+    }
+  ]
+  sync = [for v in data.kubectl_file_documents.sync.documents : {
+    data : yamldecode(v)
+    content : v
+    }
+  ]
+}
+
+resource "kubectl_manifest" "install" {
+  depends_on = [kubernetes_namespace.flux_system]
+  for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  yaml_body  = each.value
+}
+
+resource "kubectl_manifest" "sync" {
+  depends_on = [kubectl_manifest.install, kubernetes_namespace.flux_system]
+  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  yaml_body  = each.value
+}
+
+locals {
+  known_hosts = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+}
+
+resource "tls_private_key" "github_deploy_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "kubernetes_secret" "main" {
+  depends_on = [kubectl_manifest.install]
+
+  metadata {
+    name      = data.flux_sync.main.secret
+    namespace = data.flux_sync.main.namespace
+  }
+
+  data = {
+    known_hosts    = local.known_hosts
+    identity       = tls_private_key.github_deploy_key.private_key_pem
+    "identity.pub" = tls_private_key.github_deploy_key.public_key_openssh
+  }
+}
+
+# Github
+provider "github" {
+  token        = var.github_token
+  owner        = var.github_owner
+}
+
+# To make sure the repository exists and the correct permissions are set.
+data "github_repository" "main" {
+  full_name = "${var.organization}/${var.repository_name}"
+}
+
+resource "github_repository_file" "install" {
+  repository          = data.github_repository.main.name
+  file                = data.flux_install.main.path
+  content             = data.flux_install.main.content
+  branch              = var.branch
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "sync" {
+  repository          = var.repository_name
+  file                = data.flux_sync.main.path
+  content             = data.flux_sync.main.content
+  branch              = var.branch
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "kustomize" {
+  repository          = var.repository_name
+  file                = data.flux_sync.main.kustomize_path
+  content             = data.flux_sync.main.kustomize_content
+  branch              = var.branch
+  overwrite_on_create = true
+}
+
+# For flux to fetch source
+resource "github_repository_deploy_key" "flux" {
+  title      = var.github_deploy_key_title
+  repository = data.github_repository.main.name
+  key        = tls_private_key.github_deploy_key.public_key_openssh
+  read_only  = true
+}
